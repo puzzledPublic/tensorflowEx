ea85c8e629249204542b32faaec92352f3409395
crack <puzzledPublic@gmail.com>
2017-06-16 18:52:10 +0900

    ex

--- linearRegression.py
+++ linearRegression.py
@@ -16,6 +16,7 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train = optimizer.minimize(cost)
 
 sess = tf.Session()
+
 sess.run(tf.global_variables_initializer())
 
 for step in range(2001):
